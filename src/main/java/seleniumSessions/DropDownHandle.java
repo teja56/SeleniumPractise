5b88b587cf9db3db6f7d11e83ea962255ba93fed
kosaiteja <kosaiteja@deloitte.com>
2020-07-09 22:27:06 +0530

    select class update

--- src/main/java/seleniumSessions/DropDownHandle.java
+++ src/main/java/seleniumSessions/DropDownHandle.java
@@ -1,5 +1,7 @@
 package seleniumSessions;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -54,5 +56,24 @@ public class DropDownHandle {
 	public static void selectValueFromDropDownByIndex(WebElement element, int index){
 		Select select =new Select(element);
 		select.selectByIndex(index);	
+	}
+	/**
+	 * This method is used to select the values from the dropdown without select class
+	 * @param driver
+	 * @param locator
+	 * @param value
+	 */
+	public static void selectDropDownValueWithOutSelect(WebDriver driver, String locator, String value){
+		List<WebElement> dropList = driver.findElements(By.xpath(locator));
+		System.out.println(dropList.size());
+
+		for (int i = 0; i < dropList.size(); i++) {
+			String text = dropList.get(i).getText();
+			System.out.println(text);
+			if (text.equals(value)) {
+				dropList.get(i).click();
+				break;
+			}
+		}
 	}
 }
